61d36363ec3e20cfa1da2b107cacf33ece89917c
litongjava <litongjava@qq.com>
2025-03-23 10:55:59 -1000

    feat: change name to max-search

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -14,9 +14,9 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: 'Perplexica - Chat with the internet',
+  title: 'Max Search - Chat with the internet',
   description:
-    'Perplexica is an AI powered chatbot that is connected to the internet.',
+    'Max Search is an AI powered chatbot that is connected to the internet.',
 };
 
 export default function RootLayout({
--- ui/components/Chat.tsx
+++ ui/components/Chat.tsx
@@ -55,7 +55,7 @@ const Chat = ({
     messageEnd.current?.scrollIntoView({ behavior: 'smooth' });
 
     if (messages.length === 1) {
-      document.title = `${messages[0].content.substring(0, 30)} - Perplexica`;
+      document.title = `${messages[0].content.substring(0, 30)} - Max Search`;
     }
   }, [messages]);
 
